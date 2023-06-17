f659b4c54c640d9aed93ea575b350808e46a5dcd
Maybeanewbie <piedraigor@gmail.com>
2023-06-16 20:18:40 -0400

    Modified getErrorMessage function
    +Added message for status code 500
    +Removed rounding operation

--- src/Components/utils/getErrorMessage.js
+++ src/Components/utils/getErrorMessage.js
@@ -2,6 +2,7 @@ const errorMessages = {
   400: "No está autorizado para acceder a esta función o la misma no existe",
   422: "Algo está mal en su solicitud",
   429: "Demasiadas solicitudes, intente de nuevo en unos segundos",
+  500: "Algo está mal en su solicitud",
   network: "El servidor no se encuentra disponible o hay un problema de red",
 };
 
@@ -9,7 +10,7 @@ function getErrorMessages(response) {
   if (response >= 400 && response <= 420) {
     return errorMessages[400];
   } else if (typeof response === "number") {
-    return errorMessages[Math.floor(response / 100) * 100];
+    return errorMessages[response];
   } else {
     return errorMessages.network;
   }
