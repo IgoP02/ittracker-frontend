3f36c80d375f3fbd00d618222e06aa1b8b4dc6c9
Maybeanewbie <piedraigor@gmail.com>
2023-06-22 20:00:52 -0400

    removed trycatch block so components handle errors

--- src/Components/utils/fetchStats.js
+++ src/Components/utils/fetchStats.js
@@ -5,17 +5,7 @@ import { AxiosAdmin } from "./axiosClients";
  * @returns {object} response data
  */
 export default async function fetchStats(field) {
-  try {
-    const { data } = await AxiosAdmin.get(`/reports/stats/${field}`);
-    console.log(data);
-    return await data;
-  } catch (error) {
-    if (error.response) {
-      console.log(error.response);
-      return error.response.status;
-    } else if (error.message) {
-      console.log(error.message);
-      return error.message;
-    }
-  }
+  const { data } = await AxiosAdmin.get(`/reports/stats/${field}`);
+  console.log(data);
+  return await data;
 }
